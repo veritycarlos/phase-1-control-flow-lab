e35b2617a09e3de1b941aa5fa3b0cac5bd0658c3
Verity Carlos <verity.carlos@gmail.com>
2022-04-13 16:51:14 -0500

    commit tree af3ddc229fcaa3e5b9fc823e964e6ce047021a76

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(SomeDistance) {
+
+  let result
+  if (SomeDistance <= 400 ) {
+      result = "This one is on me!"; 
+      } else if (SomeDistance >=2500) { 
+        result = "No can do.";
+      } else if (SomeDistance > 2000) { 
+        result = "I will gladly take your thirty bucks.";
+      }
+  return result
+  }
+
+function ternaryCheckCity(isCity) {
+  return (isCity === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+let charmStatement;
+  switch (tip) {
+    case 'generous':
+      charmStatement = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charmStatement = 'Thank you.';
+      break;
+    default:
+      charmStatement = 'Bye.';          
+  }
+  return charmStatement;
 }
